37e124e61fba1050cb13b55a99fcae1b6ed55c04
Leonardo Omaña <leotangram@gmail.com>
2021-11-11 12:53:04 -0500

    ProductScreen Design

--- src/screens/ProductScreen.tsx
+++ src/screens/ProductScreen.tsx
@@ -1,5 +1,12 @@
 import React, { FC, useEffect } from 'react'
-import { Text, View } from 'react-native'
+import {
+  Button,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View
+} from 'react-native'
 import { StackScreenProps } from '@react-navigation/stack'
 import { ProductsStackParams } from '../navigator/ProductsNavigator'
 
@@ -16,12 +23,47 @@ const ProductScreen: FC<ProductScreenProps> = ({ navigation, route }) => {
   }, [])
 
   return (
-    <View>
-      <Text>
-        {id} {name}
-      </Text>
+    <View style={styles.container}>
+      <ScrollView>
+        <Text style={styles.label}>Nombre del producto</Text>
+        <TextInput placeholder="Producto" style={styles.textInput} />
+        <Text style={styles.label}>Categorías</Text>
+        <Button color="#5856d6" onPress={() => {}} title="Guardar" />
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'center',
+            marginTop: 10
+          }}
+        >
+          <Button color="#5856d6" onPress={() => {}} title="Cámara" />
+          <View style={{ width: 10 }} />
+          <Button color="#5856d6" onPress={() => {}} title="Galería" />
+        </View>
+      </ScrollView>
     </View>
   )
 }
 
 export default ProductScreen
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginHorizontal: 20,
+    marginTop: 10
+  },
+  label: {
+    fontSize: 18
+  },
+  textInput: {
+    borderColor: 'rgba(0, 0, 0, 0.2)',
+    borderRadius: 20,
+    borderWidth: 1,
+    height: 45,
+    marginBottom: 15,
+    marginTop: 5,
+    paddingHorizontal: 10,
+    paddingVertical: 5
+  }
+})
